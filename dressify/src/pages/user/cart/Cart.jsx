23d0a0060cf57137463fdde05b3acfb1a3173cd7
RawanAlshehri <rawan.alshehri@outlook.com>
2021-06-12 23:48:36 +0300

    Linked Cart and Order to backend

--- dressify/src/pages/user/cart/Cart.jsx
+++ dressify/src/pages/user/cart/Cart.jsx
@@ -1,63 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
+import React, { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
 import axios from "axios";
-import { useParams } from 'react-router-dom'
-import { useHistory } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import Table from 'react-bootstrap/Table';
-import Card from 'react-bootstrap/Card'
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import ItemRow from "./ItemRow";
 
-
-
-export default function Cart () {
-            return (
-                  <Container component="main" maxWidth="xs">
+export default function Cart() {
+  const userId = localStorage.getItem("user_id");
+  console.log(userId);
+  const [Items, setItems] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/cart/${userId}`)
+      .then((data) => {
+        setItems(data.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  console.log(Items.items)
+  let cartContent = "";
+  let cartEmpty = "";
+  if(Items.items == undefined) {
+    cartEmpty = "Your cart is empty you didn't add any products!"
+  } else {
+    cartContent = Items.items.map((item, i) => (
+      <ItemRow item={item} i={i++}/>
+   ));
+  }
+    
+  const onClickEmpty = () => {
+    axios
+    .delete(`http://localhost:4000/api/cart/${userId}`)
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
+    window.location.reload(); 
+  };
+   
+  return (
+    <Container component="main" maxWidth="xs">
       <div className="card user-detils">
         <div className="row">
           <div className="col-lg-4 col-sm-6" data-aos="fade-up">
             <ul className="user-list">
               <h3 className="user-title">My Cart</h3>
-              <p>Order Summary</p>
-             <hr></hr>
-              <h5><h4>Total</h4>1800 S.R</h5>
-              <a href="/Checkout"><Button style={{width: 260}}>Checkout</Button></a>
-       
+              <h5>
+                <h4>Cart Total</h4>
+                {Items.bill | 0} SR
+              </h5>
+              <a href="/Checkout">
+                <Button style={{ width: 260 }}>Checkout</Button>
+              </a>
+              <Button onClick={onClickEmpty} style={{ width: 260 }}>Empty Cart</Button>
             </ul>
           </div>
           <div className="col-lg-8 col-sm-6" data-aos="fade-up">
             <div className="card-body">
-              <h2 className="card-title">Select Payment Method</h2>
               <div className="row">
-              <Table striped bordered hover>
-                                                <thead>
-                                                      <tr>
-                                                            <th>#</th>
-                                                            <th></th>
-                                                            <th>Product</th>
-                                                            <th>QTY</th>
-                                                            <th>Total</th>
-                                                      </tr>
-                                                </thead>
-                                                <tbody>
-                                                      <tr>
-                                                            <td>1</td>
-                                                            <td> <Card.Img style={{width: 100}} src="https://ounass-prod1.atgcdn.ae/small_light(p=zoom,of=webp,q=65)/pub/media/catalog/product/2/1/214217820_black_in.jpg?1610642647.2564"></Card.Img></td>
-                                                            <td>Fendi Dress</td>
-                                                            <td>2</td>
-                                                            <td>1800 S.R</td>
-                                                      </tr>
-                                                    
-                                                </tbody>
-                                          </Table>
+                {cartEmpty}
+                <Table  striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Product</th>
+                      <th>Price</th>
+                      <th>QTY</th>
+                      <th>Total</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartContent}
+                  </tbody>
+                </Table>
               </div>
             </div>
           </div>
         </div>
       </div>
-
-
     </Container>
-
-            )
-      }
-
+  );
+}
--- dressify/src/pages/user/profile/Order.jsx
+++ dressify/src/pages/user/profile/Order.jsx
@@ -1,66 +1,88 @@
-import React, { Component } from 'react'
-import { useHistory } from "react-router-dom"
-import Container from 'react-bootstrap/Container'
-import Button from 'react-bootstrap/Button'
-import "../../../assets/css/profile.css"
-import { BsBag } from 'react-icons/bs';
-import Table from 'react-bootstrap/Table'
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import "../../../assets/css/profile.css";
+import { BsBag } from "react-icons/bs";
+import Table from "react-bootstrap/Table";
+import axios from "axios";
+import OrderRow from './OrderRow';
 
 
 export default function Order({ user, Login }) {
+  const history = useHistory();
+  const [orders, setOrders] = useState([]);
+  const [modalShow, setModalShow] = useState(false);
+ 
+  // user/seller id
+  const id = localStorage.getItem("user_id");
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/api/order/${id}`)
+      .then((data) => {
+        setOrders(data.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-      const history = useHistory()
-
-      const logOut = () => {
-            localStorage.removeItem("token")
-            Login()
-            history.push("/")
-
-      }
-      return (
-            <Container component="main" maxWidth="xs">
-                  <div className="card user-detils">
-                        <div className="row">
-                              <div className="col-lg-4 col-sm-6" data-aos="fade-up">
-                                    <ul className="user-list">
-                                          <h3 className="user-title">My Account</h3>
-                                          <a href="/Order"><li>My Orders</li></a>
-                                          <a href="/Profile"><li>My Profile</li></a>
-                                          <a href="/Address"><li>My Address Book</li></a>
-                                          <a href="/Credit"><li>Credit / Debit Cards</li></a>
-                                    </ul>
-                              </div>
-                              <div className="col-lg-8 col-sm-6" data-aos="fade-up">
-                                    <div className="card-body">
-                                          <span className="card-title">My Orders</span>
-                                          <h2 style={{ fontSize: 70 }}><BsBag /></h2>
-                                          <p>You haven't placed an order yet.</p>
-                                          <Button href="/">Shop Now</Button>
-
-                                          <Table striped bordered hover>
-                                                <thead>
-                                                      <tr>
-                                                            <th>#</th>
-                                                            <th>Product</th>
-                                                            <th>Total</th>
-                                                            <th>Status</th>
-                                                      </tr>
-                                                </thead>
-                                                <tbody>
-                                                      <tr>
-                                                            <td>1</td>
-                                                            <td>item1</td>
-                                                            <td>638 S.R</td>
-                                                            <td style={{fontWeight: 'bold'}}>Delivered</td>
-                                                      </tr>
-                                                    
-                                                </tbody>
-                                          </Table>
-                                    </div>
-                              </div>
-                        </div>
-                  </div>
-            </Container>
-      )
-
+  let orderContent = "";
+  let orderEmpty = "";
+  if(orders == undefined) {
+     orderEmpty = "You haven't placed any orders yet."
+  } else {
+      orderContent = orders.map((order) =><OrderRow order={order}/>);
+  }
+  const logOut = () => {
+    localStorage.removeItem("token");
+    Login();
+    history.push("/");
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className="card user-detils">
+        <div className="row">
+          <div className="col-lg-4 col-sm-6" data-aos="fade-up">
+            <ul className="user-list">
+              <h3 className="user-title">My Account</h3>
+              <a href="/Order">
+                <li>My Orders</li>
+              </a>
+              <a href="/Profile">
+                <li>My Profile</li>
+              </a>
+              <a href="/Address">
+                <li>My Address Book</li>
+              </a>
+              <a href="/Credit">
+                <li>Credit / Debit Cards</li>
+              </a>
+            </ul>
+          </div>
+          <div className="col-lg-8 col-sm-6" data-aos="fade-up">
+            <div className="card-body">
+              <span className="card-title">My Orders</span>
+              <h2 style={{ fontSize: 70 }}>
+                <BsBag />
+              </h2>
+              <p>{orderEmpty}</p>
+              <Button href="/">Shop Now</Button>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>Order Number</th>
+                    <th>Total Price</th>
+                    <th>Order Details</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {orderContent}
+                </tbody>
+              </Table>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <OrderDetails show={modalShow} onHide={() => setModalShow(false)} />  */}
+    </Container>
+  );
 }
